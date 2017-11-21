dcf1e22051d816c1ae2fd411a3149d723be5824a
sghuangrihuang <287545836@qq.com>
2017-11-21 19:36:12 +0800

    上传20171121 node接口测试

---
+++ node/20171121/ApiServer/routes/user.js
@@ -0,0 +1,8 @@
+function User() {
+  this.name;
+  this.city;
+  this.age;
+  this.token = 1;
+}
+
+module.exports = User;
---
+++ node/20171121/ApiServer/routes/users.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var URL = require('url');
+var User = require('./user');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/getUserInfo', function(req, res, next) {
+  var user = new User();
+  var params = URL.parse(req.url, true).query;
+
+  if (params.id == 1) {
+    user.name = 'user';
+    user.age = 1;
+    user.city = '广州';
+  } else {
+    user.name = 'admin';
+    user.age = 10;
+    user.city = '上海';
+  }
+  var response = { 
+    status: 1,
+    data: user
+  };
+  
+  res.send(JSON.stringify(response));
+
+})
+
+
+module.exports = router;
